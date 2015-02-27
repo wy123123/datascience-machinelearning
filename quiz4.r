5c4a137b892503851577b585702422293c742023
wy123123 <wangyu0917@hotmail.com>
2015-02-27 18:35:08 +0800

    Create quiz4.r

---
+++ quiz4.r
@@ -0,0 +1,46 @@
+
+library(ElemStatLearn)
+data(vowel.train)
+data(vowel.test)
+set.seed(33833)
+head(vowel.train)
+vowel.train$y=as.factor(vowel.train$y)
+vowel.test$y=as.factor(vowel.test$y)
+modfit=train(y~.,method="rf",data=vowel.train)
+confusionMatrix(vowel.test$y,predict(modfit,vowel.test))
+modfit.2=train(y~.,method="gbm",data=vowel.train,verbose=F)
+confusionMatrix(vowel.test$y,predict(modfit.2,vowel.test))
+
+
+
+library(caret)
+library(gbm)
+set.seed(62433)
+library(AppliedPredictiveModeling)
+data(AlzheimerDisease)
+adData = data.frame(diagnosis,predictors)
+inTrain = createDataPartition(adData$diagnosis, p = 3/4)[[1]]
+training = adData[ inTrain,]
+testing = adData[-inTrain,]
+modfit.1=train(diagnosis~.,method="rf",data=training)
+modfit.2=train(diagnosis~.,method="gbm",data=training,verbose=F)
+modfit.3=train(diagnosis~.,method="lda",data=training)
+confusionMatrix(testing$diagnosis,predict(modfit.1,testing))
+confusionMatrix(testing$diagnosis,predict(modfit.2,testing))
+confusionMatrix(testing$diagnosis,predict(modfit.3,testing))
+pred.1=predict(modfit.1,testing)
+pred.2=predict(modfit.2,testing)
+pred.3=predict(modfit.3,testing)
+comb=data.frame(pred.1,pred.2,pred.3,diagnosis=testing$diagnosis)
+combfit=train(diagnosis~.,method="rf",data=comb)
+confusionMatrix(testing$diagnosis,predict(combfit,comb))
+
+
+set.seed(233)
+library(AppliedPredictiveModeling)
+data(concrete)
+inTrain = createDataPartition(concrete$CompressiveStrength, p = 3/4)[[1]]
+training = concrete[ inTrain,]
+testing = concrete[-inTrain,]
+modfit=train(CompressiveStrength~.,method="lasso",data=training)
+varImp(modfit)
