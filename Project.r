51a85ef251a6808f29d1addc0b9739bd2b0f4613
wy123123 <wangyu0917@hotmail.com>
2015-03-19 16:43:12 +0800

    Update Project.r

--- Project.r
+++ Project.r
@@ -18,8 +18,8 @@ dt.test <- read.csv(file)
 dt.train.1 <- subset(dt.train,select=-c(X,user_name,raw_timestamp_part_1,raw_timestamp_part_2,cvtd_timestamp,num_window))
 dt.test.1 <-subset(dt.test,select=-c(X,user_name,raw_timestamp_part_1,raw_timestamp_part_2,cvtd_timestamp,num_window)) 
 #remove NA columns
-dt.train.2=dt.1[,colSums(is.na(dt.1))<19622*0.1]
-dt.test.2=dt.1[,colSums(is.na(dt.1))<20*0.1]
+dt.train.2=dt.train.1[,colSums(is.na(dt.1))<19622*0.1]
+dt.test.2=dt.test.1[,colSums(is.na(dt.1))<20*0.1]
 #near near 0 variance columns
 dt.train.3 <- nearZeroVar(dt.train.2,saveMetrics=T)
 dt.train.4 <- dt.train.2[,!dt.train.3$nzv]
@@ -34,13 +34,13 @@ pre <- preProcess(dt.train.4.v,method="pca")
 trainP=predict(pre,dt.train.4.v)
 testP=predict(pre,dt.test.4.v)
 
-#training model
+#model selection
 mod.1 <- train(dt.train.4$classe~.,method="rf",data=trainP)
 mod.2 <- train(dt.train.4$classe~.,method="gbm",data=trainP)
 mod.3 <- train(dt.train.4$classe~.,method="rpart",data=trainP)
 mod.4 <- train(dt.train.4$classe~.,method="lda",data=trainP)
 
-confusionMatrix(dt.test.4$classe,predict(mod.3,testP))
+confusionMatrix(dt.train.4$classe,predict(mod.4,trainP))
 varImp(mod.1)
 
 #random forest is the most accurate model.
@@ -50,16 +50,16 @@ varImp(mod.1)
 ##################
 set.seed(123)
 #create 10 folds, find the average sample error of the 10 sets of data
-folds.train <- createFolds(y=dt.train.4$classe,k=10,list=T,returnTrain=T)
-folds.test<- createFolds(y=dt.train.4$classe,k=10,list=T,returnTrain=F)
+folds.train <- createFolds(y=dt.train$classe,k=10,list=T,returnTrain=T)
+folds.test<- createFolds(y=dt.train$classe,k=10,list=T,returnTrain=F)
 cmatrix=0
 for(i in 1 : 10){
         
-        dt.train <- subset(dt.train[folds.train[[i]],],select=-c(X,user_name,raw_timestamp_part_1,raw_timestamp_part_2,cvtd_timestamp,num_window))
+        dt.train.1 <- subset(dt.train[folds.train[[i]],],select=-c(X,user_name,raw_timestamp_part_1,raw_timestamp_part_2,cvtd_timestamp,num_window))
         dt.test.1 <-subset(dt.train[folds.test[[i]],],select=-c(X,user_name,raw_timestamp_part_1,raw_timestamp_part_2,cvtd_timestamp,num_window)) 
         #remove NA columns
-        dt.train.2=dt.1[,colSums(is.na(dt.1))<length(folds.train[[i]])*0.1]
-        dt.test.2=dt.1[,colSums(is.na(dt.1))<length(folds.test[[i]])*0.1]
+        dt.train.2=dt.train.1[,colSums(is.na(dt.1))<length(folds.train[[i]])*0.1]
+        dt.test.2=dt.test.1[,colSums(is.na(dt.1))<length(folds.test[[i]])*0.1]
         #near near 0 variance columns
         dt.train.3 <- nearZeroVar(dt.train.2,saveMetrics=T)
         dt.train.4 <- dt.train.2[,!dt.train.3$nzv]
@@ -77,5 +77,3 @@ for(i in 1 : 10){
         mod <- train(dt.train.4$classe~.,method="rf",data=trainP)
         cmatrix[i]=confusionMatrix(dt.test.4$classe,predict(mod.3,testP))
 }
-
-avg.accurate=
