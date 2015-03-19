8e62912693adcbed7554d060e1e58575b3c5a53b
wy123123 <wangyu0917@hotmail.com>
2015-03-19 16:06:45 +0800

    Update Project.r

--- Project.r
+++ Project.r
@@ -43,10 +43,39 @@ mod.4 <- train(dt.train.4$classe~.,method="lda",data=trainP)
 confusionMatrix(dt.test.4$classe,predict(mod.3,testP))
 varImp(mod.1)
 
+#random forest is the most accurate model.
+
 ##################
-#k-fold
+#Cross validation, k-fold 
 ##################
 set.seed(123)
-folds <- createFolds(y=dt.train.4$classe,k=10,list=T,returnTrain=T)
-sapply(folds,length)
-folds
+#create 10 folds, find the average sample error of the 10 sets of data
+folds.train <- createFolds(y=dt.train.4$classe,k=10,list=T,returnTrain=T)
+folds.test<- createFolds(y=dt.train.4$classe,k=10,list=T,returnTrain=F)
+cmatrix=0
+for(i in 1 : 10){
+        
+        dt.train <- subset(dt.train[folds.train[[i]],],select=-c(X,user_name,raw_timestamp_part_1,raw_timestamp_part_2,cvtd_timestamp,num_window))
+        dt.test.1 <-subset(dt.train[folds.test[[i]],],select=-c(X,user_name,raw_timestamp_part_1,raw_timestamp_part_2,cvtd_timestamp,num_window)) 
+        #remove NA columns
+        dt.train.2=dt.1[,colSums(is.na(dt.1))<length(folds.train[[i]])*0.1]
+        dt.test.2=dt.1[,colSums(is.na(dt.1))<length(folds.test[[i]])*0.1]
+        #near near 0 variance columns
+        dt.train.3 <- nearZeroVar(dt.train.2,saveMetrics=T)
+        dt.train.4 <- dt.train.2[,!dt.train.3$nzv]
+        dt.test.3 <- nearZeroVar(dt.test.2,saveMetrics=T)
+        dt.test.4 <- dt.test.2[,!dt.test.3$nzv]
+        #remove the response variable
+        dt.train.4.v <- subset(dt.train.4,select=-classe)
+        dt.test.4.v <- subset(dt.test.4,select=-classe)
+        #PCA for further dimention reduction
+        pre <- preProcess(dt.train.4.v,method="pca")
+        #tansforme data
+        trainP=predict(pre,dt.train.4.v)
+        testP=predict(pre,dt.test.4.v)
+        
+        mod <- train(dt.train.4$classe~.,method="rf",data=trainP)
+        cmatrix[i]=confusionMatrix(dt.test.4$classe,predict(mod.3,testP))
+}
+
+avg.accurate=
